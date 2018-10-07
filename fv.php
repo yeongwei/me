2e7584c50377e377d508645931c9a6324a62452c
YeongWei <ywlai@origineit.com>
2018-10-07 16:41:29 +0800

    WIP

--- fv.php
+++ fv.php
@@ -12,6 +12,14 @@ $remainingYearsToRetirement = 25;
 $currentAmount = 5000.00;
 
 echo "At year " . $currentYear . ". Inflation Rate of " . $inflationRate * 100 . "% for a period of " . $remainingYearsToRetirement . " years of RM " . $currentAmount . ".";
+
+$investments = array(
+	array("name" => "3%", "rate" => 0.03, "value" => $currentAmount),
+	array("name" => "5%", "rate" => 0.05, "value" => $currentAmount),
+	array("name" => "7%", "rate" => 0.07, "value" => $currentAmount),
+	array("name" => "8%", "rate" => 0.08, "value" => $currentAmount),
+	array("name" => "9%", "rate" => 0.09, "value" => $currentAmount),
+	array("name" => "10%", "rate" => 0.10, "value" => $currentAmount),);
 ?>
 
 <table border="1">
@@ -23,19 +31,19 @@ echo "At year " . $currentYear . ". Inflation Rate of " . $inflationRate * 100 .
 		<th>Inflated Amount</th>
 		<th>Depreciated Amount</th>
 		<th>New Amount</th>
-		<th>ROI 3%</th>
-		<th>ROI 5%</th>
+		<?php foreach ($investments as $k => $r) { ?>
+		<th>ROI <?php echo $r["name"]; ?></th>
+		<?php } ?>
 	</tr>
 <?php
 	$newAmount	= $currentAmount;
-	$amountFor3 = $currentAmount;
-	$amountFor5 = $currentAmount;
 	for ($i = 0; $i <= $remainingYearsToRetirement; $i++) {
-		$inflatedAmount = $currentAmount * (1 - $inflationRate);
-		$depreciatedAmount = $currentAmount - $inflatedAmount;
-		$newAmount = $newAmount + ($newAmount *$inflationRate);
-		$amountFor3 = $amountFor3 * (1 - $inflationRate + 0.03);
-		$amountFor5 = $amountFor5 * (1 - $inflationRate + 0.05);
+		$inflatedAmount = $currentAmount * (1 - $inflationRate); // Remaining value adjusted by inflation
+		$depreciatedAmount = $currentAmount - $inflatedAmount; // Value depreciated by inflation
+		$newAmount = $newAmount + ($newAmount * $inflationRate); // Required amount to fend of inflation
+		foreach ($investments as $k => $r)
+			$investments[$k]["value"] = $investments[$k]["value"] * 
+			(1 - $inflationRate + $investments[$k]["rate"])
 ?>
 	<tr>
 		<td><?php echo $i + 1?></td>
@@ -45,8 +53,9 @@ echo "At year " . $currentYear . ". Inflation Rate of " . $inflationRate * 100 .
 		<td><?php echo rounding($inflatedAmount); ?></td>
 		<td><?php echo rounding($depreciatedAmount); ?></td>
 		<td><?php echo rounding($newAmount); ?></td>
-		<td><?php echo rounding($amountFor3); ?></td>
-		<td><?php echo rounding($amountFor5); ?></td>
+		<?php foreach ($investments as $k => $r) { ?>
+		<td><?php echo rounding($investments[$k]["value"]); ?></td>
+		<?php } ?>
 	</tr>
 <?php
 		$currentAmount = $inflatedAmount;
